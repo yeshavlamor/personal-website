e69437a40fb319a1c8b5ea6d32cc3895feb01f3a
yesha <yeshavlamor@gmail.com>
2025-09-08 18:21:20 +0800

    edit instructions

--- src/components/IntroScramble.jsx
+++ src/components/IntroScramble.jsx
@@ -356,9 +356,7 @@ useEffect(() => {
 						})
 					})()}
 				</motion.div>
-                {/* author name? */}
-                {/* <div className="mt-6 sm:text-3xl md:text-4xl lg:text-5xl font-serif text-glow">―Og Mandino</div> */}
-				<div className="mt-6 text-sm opacity-60">Scramble the quote with your mouse or double tap</div>
+				<div className="mt-6 text-sm opacity-60">Scramble with your mouse or tap</div>
 			</div>
 		</section>
 	)
