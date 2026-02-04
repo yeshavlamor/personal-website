852a4064a54de01e03b596878bde2fc10970948c
yeshavlamor <yeshavlamor@gmail.com>
2026-02-04 15:43:42 +0800

    bottom intro text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,21 +10,34 @@ export const HeroSection = () => {
             <div className="container max-w-6xl mx-auto z-10">
                 <div className="flex flex-col lg:flex-row items-center justify-between gap-6 md:gap-8 lg:gap-12">
                     
-                    {/* left child: Text Content */}
+                    {/* left child: Profile Picture */}
+                    <div className="grow-[1] justify-center lg:justify-end opacity-0 animate-fade-in-delay-3">
+                        <div className="relative">
+                            <div className="w-40 h-40 sm:w-66 sm:h-66 md:w-72 md:h-72 rounded-full overflow-hidden border-4 border-primary/20 shadow-2xl">
+                                <img 
+                                    src="/projects/LinkedIn_Photo.jpeg" 
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* right child: Text Content */}
                     <div className="grow-[2] text-left space-y-4 md:space-y-6">
                         <h1 className="text-xl sm:text-3xl md:text-4xl font-bold tracking-tight whitespace-nowrap">
                             <span className="opacity-0 animate-fade-in-delay-2"> Hi there! My name is Ayesha Villamor.</span>
                         </h1> 
 
-                        <h2 className="text-md sm:text-lg md:text-2xl font-medium animate-fade-in-delay-3 text-left">
+                        <h2 className="text-sm sm:text-md md:text-xl font-medium animate-fade-in-delay-3 text-left">
                             <span className="block mt-1">Double Degree in Business & Computer Science </span> 
-                            <span className="block mt-1">NTU Singapore</span>
-                            <span className="block mt-1">NCSS Scholar</span>
+                            <span className="block mt-1">NTU, Singapore</span>
+                            {/* <span className="block mt-1">NCSS Scholar</span> */}
                         </h2>
 
-                        {/* writings button */}
-                        <div className="pt-4 opacity-0 animate-fade-in-delay-5 space-y-4">
-                            <div className="flex flex-row items-center justify-center lg:justify-start gap-3 md:gap-4">
+                        {/* buttons */}
+                        <div className="pt-4 opacity-0 animate-fade-in-delay-5 space-y-4">  
+                            <div className="flex flex-row items-left lg:justify-start gap-3 md:gap-4">
+                                {/* writings button */}
                                 <Link 
                                     to="/writings" 
                                     className="px-4 py-2 md:px-6 md:py-3 rounded-full bg-primary/10 text-primary font-medium text-sm md:text-base 
@@ -71,19 +84,6 @@ export const HeroSection = () => {
 
                     </div>
 
-                    {/* right child: Profile Picture */}
-                    <div className="grow-[1] justify-center lg:justify-end opacity-0 animate-fade-in-delay-3">
-                        <div className="relative">
-                            <div className="w-30 h-30 sm:w-46 sm:h-46 md:w-62 md:h-62 rounded-full overflow-hidden border-4 border-primary/20 shadow-2xl">
-                                <img 
-                                    src="/projects/LinkedIn_Photo.jpeg" 
-                                    className="w-full h-full object-cover"
-                                />
-                            </div>
-                            {/* Add a subtle glow effect */}
-                            <div className="absolute inset-0 rounded-full bg-gradient-to-r from-primary/20 to-primary/10 blur-xl -z-10"></div>
-                        </div>
-                    </div>
                 </div>
 
                 {/* bottom intro */}                
@@ -92,9 +92,20 @@ export const HeroSection = () => {
                             I am always keen on learning new things! As of now, 
                             I am currently exploring the intersection of AI, Big Data and Sustainability.
                         </p> */}
-                        <p> I’m particularly interested in artificial intelligence, machine learning, and building systems that are both technically sound and human-centred. Whether it’s experimenting with models, designing dashboards, or exploring how technology can be applied to sustainability and real-world problems, I enjoy learning by building and questioning along the way.
-
-Beyond the technical side, I’m someone who’s constantly exploring — ideas, places, and creative outlets. I care deeply about growth, curiosity, and doing work that feels meaningful, even when the path isn’t fully defined yet. This site is less about having everything figured out, and more about documenting the process. Thank you for visiting, I hope you enjoy your time here! 
+                        <p> Somewhere between training my first model and first few merge conflicts, 
+                            I realised I cared not just about how to build systems, but why we even build them. 
+                            
+                            The more I built, the more I noticed that good technology isn’t just powerful — it’s considerate.
+                            
+                            Nice to meet you! I am a Final Year Undergrad, with curiosity spanning tech and everything outside it. 
+                            I am currently exploring the realms of AI and Big Data, with special interest in building technology 
+                            for social good and sustainability.
+{/*                             
+                            Beyond tech, I am someone who is constantly exploring — ideas, places, and creative outlets. 
+                            I care deeply about growth, curiosity, and doing work that feels meaningful, 
+                            even when the path isn’t fully defined yet. 
+                            This site is less about having everything figured out, and more about documenting my journey through it all. 
+                            Thank you for visiting, I hope you enjoy your time here!  */}
 
                         </p>
                     </div>
